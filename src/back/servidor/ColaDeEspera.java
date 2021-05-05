8b0afca1007bce025f74d0d4472df3ae0d601f6f
MatiasC248 <MatiasC248@users.noreply.github.com>
2021-05-05 14:09:30 -0300

    ListaDeTurnos y Cola

--- src/back/servidor/ColaDeEspera.java
+++ src/back/servidor/ColaDeEspera.java
@@ -3,10 +3,14 @@ package back.servidor;
 import java.util.PriorityQueue;
 
 public class ColaDeEspera extends PriorityQueue<String> {
+	public PriorityQueue<String> a = new PriorityQueue<String>();
 	
-	
-	
-	
-	
-	
+	public void algo() {
+		a.add(string e)
+		a.contains(object o)
+	}
+	@Override
+	public void add(String e) {
+		super.add(e);
+	}
 }
--- src/back/servidor/ListaDeTurnos.java
+++ src/back/servidor/ListaDeTurnos.java
@@ -1,5 +1,17 @@
 package back.servidor;
 
-public class ListaDeTurnos {
+import java.util.ArrayList;
+import java.util.HashMap;
 
+public class ListaDeTurnos {
+	//private ArrayList<Turno> lista = new ArrayList<Turno>();
+	private HashMap<Integer,String> lista = new HashMap<Integer,String>();
+	
+	public void añadirTurno(Turno turno) {
+		this.lista.put(turno.getPuesto(), turno.getDni());
+	}
+	public void eliminarTurno(Integer puesto) {
+		this.lista.remove(puesto); 
+	}
+	
 }
