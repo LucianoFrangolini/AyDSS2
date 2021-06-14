0eaa57ae2f182a5899575ead5611afd3c9e5144c
LucianoFrangolini <64044827+LucianoFrangolini@users.noreply.github.com>
2021-06-13 22:08:51 -0300

    documentacion restante

--- Servidor/back/servidor/persistencia/Registro.java
+++ Servidor/back/servidor/persistencia/Registro.java
@@ -2,14 +2,21 @@ package back.servidor.persistencia;
 
 import java.io.Serializable;
 
-public class Registro implements Serializable{
+/**
+ * Clase Registro contiene la informacion de cuando un cliente se registro, y su
+ * DNI, implementa Serializable</br>
+ * para su persistencia.
+ * 
+ */
+public class Registro implements Serializable {
 
 	private static final long serialVersionUID = 6287571224468610359L;
 	private String dni;
 	private String fecha;
-	
-	public Registro() {}
-	
+
+	public Registro() {
+	}
+
 	public Registro(String dni) {
 		this.dni = dni;
 		this.fecha = java.util.Calendar.getInstance().getTime().toString();
@@ -33,10 +40,7 @@ public class Registro implements Serializable{
 
 	@Override
 	public String toString() {
-		return "[ Dni : " + dni + " || Fecha = " + fecha +" ]";
+		return "[ Dni : " + dni + " || Fecha = " + fecha + " ]";
 	}
-	
-	
-	
-	
+
 }
